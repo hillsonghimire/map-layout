4537285365af389ab6da2f9e482aa0a1ab278a39
roger-anish <rfederer75@gmail.com>
2021-04-15 17:54:02 +0545

    created view

--- mapLayout/layoutApp/views.py
+++ mapLayout/layoutApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from .models import District
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def District(request):
+    districtData = serialize('geojson',District.objects.all())  #conversion of data to geojson format
+    return HttpResponse(districtData,content_type='geojson')
