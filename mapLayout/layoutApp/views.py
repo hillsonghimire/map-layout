a6f537cc3d5538d6a4e456762f1c915273ba595c
Hillson Ghimire <ghimirehillson@gmail.com>
2021-04-17 00:18:58 +0545

    post request successful

--- mapLayout/layoutApp/views.py
+++ mapLayout/layoutApp/views.py
@@ -3,11 +3,22 @@ from django.core.serializers import serialize
 from django.http import HttpResponse
 from .models import District
 from . import templates
+import json
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'layoutApp/index.html')
+    return render(request, 'layoutApp/index.html')
+
 
 def district(request):
     districtData = serialize('geojson', District.objects.all())
     return HttpResponse(districtData, content_type='application/geojson')
+
+
+def selectDistrict(request):
+    if request.method == "POST":
+        name = json.loads(request.body.decode("utf-8"))
+        print(name['featureName'])
+    return HttpResponse("Transfer Complete. Data successfully Received.")
